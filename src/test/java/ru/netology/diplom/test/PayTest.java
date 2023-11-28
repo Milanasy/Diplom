d98ceb36a99338ff8938a73d3b7c89eed41609af
Anastasiia Rakhmanova <rar.anastasiya@gmail.com>
2023-11-28 16:20:15 +0200

    Adding Screenshots

--- src/test/java/ru/netology/diplom/test/PayTest.java
+++ src/test/java/ru/netology/diplom/test/PayTest.java
@@ -47,8 +47,6 @@ public class PayTest {
         Assertions.assertEquals("APPROVED", statusPay.getStatus());
     }
 
-
-
     @Test
     public void shouldNotConfirmCardWithDeclineCard() {
         PayPage payPage = new PayPage();
